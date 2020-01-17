54d1a201afdd4d410f0ad8f1e1321e19f533ac6f
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-01-17 11:04:01 +0100

    hitbox finally works

--- pickups/key.gd
+++ pickups/key.gd
@@ -4,7 +4,12 @@ func _ready():
 	connect("body_entered",self,"body_entered")
 
 func body_entered(body):
-	if body.name == "player" && body.get("keys") < 9:
+	print("test")
+	if body.name == "Player" && body.get("keys") < 9:
 		body.keys += 1
 		queue_free()
+
+
+
+
 
--- tiles/key_door.gd
+++ tiles/key_door.gd
@@ -4,6 +4,6 @@ func _ready():
 	$area.connect("body_entered",self,"body_entered")
 
 func body_entered(body):
-	if body.name == "player" && body.get("keys") > 0:
+	if body.name == "Player" && body.get("keys") > 0:
 		body.keys -= 1
 		queue_free()
